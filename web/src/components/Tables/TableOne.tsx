04b812e5b1bb5554aa5624dbb41c11ff44d0d9a7
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-08-26 01:49:50 +0500

    Dashboard added

---
+++ web/src/components/Tables/TableOne.tsx
@@ -0,0 +1,117 @@
+import { BRAND } from "@/types/brand";
+import Image from "next/image";
+
+const brandData = [
+  {
+    name: "React form Validator",
+    visitors: 35,
+    revenues: "5",
+    sales: 2,
+    // conversion: 4.8,
+  },
+  {
+    name: "Pantry Tracker",
+    visitors: 22,
+    revenues: "4",
+    sales: 1,
+    // conversion: 4.3,
+  },
+  {
+    name: "AI-Customer",
+    visitors: 21,
+    revenues: "2",
+    sales: 4,
+    // conversion: 3.7,
+  },
+  {
+    name: "AI-FlashCard",
+    visitors: 15,
+    revenues: "3",
+    sales: 4,
+    // conversion: 2.5,
+  },
+  {
+    name: "Code Guardian",
+    visitors: 12,
+    revenues: "1",
+    sales: 4,
+    // conversion: 1.9,
+  },
+];
+
+const TableOne = () => {
+  return (
+    <div className="rounded-[10px] bg-white px-7.5 pb-4 pt-7.5 shadow-1 dark:bg-gray-dark dark:shadow-card">
+      <h4 className="mb-5.5 text-body-2xlg font-bold text-dark dark:text-white">
+        Top Repositories
+      </h4>
+
+      <div className="flex flex-col">
+        <div className="grid grid-cols-3 sm:grid-cols-4">
+          <div className="px-2 pb-3.5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Source
+            </h5>
+          </div>
+          <div className="px-2 pb-3.5 text-center">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Commits
+            </h5>
+          </div>
+          <div className="px-2 pb-3.5 text-center">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Issues
+            </h5>
+          </div>
+          <div className="hidden px-2 pb-3.5 text-center sm:block">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Collaborators
+            </h5>
+          </div>
+          {/* <div className="hidden px-2 pb-3.5 text-center sm:block">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Conversion
+            </h5>
+          </div> */}
+        </div>
+
+        {brandData.map((brand, key) => (
+          <div
+            className={`grid grid-cols-3 sm:grid-cols-4 ${
+              key === brandData.length - 1
+                ? ""
+                : "border-b border-stroke dark:border-dark-3"
+            }`}
+            key={key}
+          >
+            <div className="flex items-center gap-3.5 px-2 py-4">
+              <p className="hidden font-medium text-dark dark:text-white sm:block">
+                {brand.name}
+              </p>
+            </div>
+
+            <div className="flex items-center justify-center px-2 py-4">
+              <p className="font-medium text-dark dark:text-white">
+                {brand.visitors}
+              </p>
+            </div>
+
+            <div className="flex items-center justify-center px-2 py-4">
+              <p className="font-medium text-red">
+                {brand.revenues}
+              </p>
+            </div>
+
+            <div className="hidden items-center justify-center px-2 py-4 sm:flex">
+              <p className="font-medium text-dark dark:text-white">
+                {brand.sales}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TableOne;
